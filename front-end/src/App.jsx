e0b372a26c1864a91dc0b6d23eb8421e9e362b1d
VHJP <vhjp@TECHIN.INTERNAL>
2025-07-23 08:28:12 +0300

    Sūkuriau navbar'ą, prisijungimo bei registracijos formas front-end'e

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css';
+import RoutePaths from "./components/RoutePaths";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <RoutePaths/>
+    </>
+  )
+}
+
+export default App
